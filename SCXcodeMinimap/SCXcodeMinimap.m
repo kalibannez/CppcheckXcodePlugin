2d2de4dbb11f43550cb31a7a4951030fcf662ec5
Перепелицын Александр <a.perepelitsyn@corp.mail.ru>
2014-03-15 21:39:31 +0400

    Cppcheck Xcode Plugin initial commit

---
+++ SCXcodeMinimap/SCXcodeMinimap.m
@@ -0,0 +1,114 @@
+//
+//  SCXcodeMinimap.m
+//  SCXcodeMinimap
+//
+//  Created by Stefan Ceriu on 3/30/13.
+//  Copyright (c) 2013 Stefan Ceriu. All rights reserved
+//
+
+#import "SCMiniMapView.h"
+#import "SCXcodeMinimap.h"
+#import <objc/runtime.h>
+
+static NSString * const IDEEditorDocumentWillSaveNotification = @"IDEEditorDocumentWillSaveNotification";
+static NSString * const PBXProjectDidOpenNotification = @"PBXProjectDidOpenNotification";
+
+@implementation SCXcodeMinimap
+
+static SCXcodeMinimap *sharedMinimap = nil;
++ (void)pluginDidLoad:(NSBundle *)plugin {
+	static dispatch_once_t onceToken;	
+	dispatch_once(&onceToken, ^{
+		sharedMinimap = [[self alloc] init];
+	});
+}
+
+- (id)init {
+	if (self = [super init]) {
+		//[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(some:) name:nil object:nil];
+		
+		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(openProject:) name:PBXProjectDidOpenNotification object:nil];
+		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(saveDocument:) name:IDEEditorDocumentWillSaveNotification object:nil];
+    }
+	return self;
+}
+
+-(void) some:(NSNotification*) sender {
+	//NSLog(@"  Notification: %@", [sender name]);
+	if ([[sender name] length] >= 2 && [[[sender name] substringWithRange:NSMakeRange(0, 2)] isEqualTo:@"NS"]) {
+		return;
+	} else {
+		NSLog(@"  Notification: %@", [sender name]);
+	}
+}
+id project = nil;
+-(void) openProject:(NSNotification*) sender {
+	if ([[NSApp mainMenu] itemWithTitle:@"View"] == nil) {
+		NSLog(@"Don't calculate derived data folder");
+		return;
+	}
+	 
+	if (![[sender object] respondsToSelector:@selector(projectFilePath)]) {
+		NSLog(@"ERROR: sender not supported event");
+		return;
+	}
+	if (project == nil) project = [sender object];
+	NSString* pbxprojDirPath = [[sender object] performSelector:@selector(projectFilePath)];
+	NSString* projectDirPath = [pbxprojDirPath substringToIndex:[pbxprojDirPath rangeOfString:@"/" options:NSBackwardsSearch].location];
+	
+	NSString* xcodebuildOutputFile = [NSString stringWithFormat:@"%@%@", NSTemporaryDirectory(), @"CppCheckIncremental.tmp"];
+	
+	NSString* cmd = [NSString stringWithFormat:@"/usr/bin/xcodebuild -project %@ -showBuildSettings > %@",projectDirPath, xcodebuildOutputFile];
+	system([cmd UTF8String]);
+	
+	NSString *xcodeBuildOutput = [NSString stringWithContentsOfFile:xcodebuildOutputFile encoding:NSUTF8StringEncoding error:nil];
+	
+	NSRange tempDirRange = [xcodeBuildOutput rangeOfString:@"PROJECT_TEMP_ROOT = "];
+	if (tempDirRange.location == NSNotFound) {
+		NSLog(@"ERROR: can't find PROJECT_TEMP_ROOT in xcodebuild output");
+		return;
+	}
+	
+	NSRange newLineSearchRange = NSMakeRange(tempDirRange.location, [xcodeBuildOutput length]-tempDirRange.location);
+	NSRange newLineRange = [xcodeBuildOutput rangeOfString:@"\n" options:NSCaseInsensitiveSearch range:newLineSearchRange];
+	if (newLineRange.location == NSNotFound) {
+		NSLog(@"ERROR: can't find new line in xcodebuild output");
+		return;
+	}
+	
+	NSString* openedProjectTempDir = [xcodeBuildOutput substringWithRange:NSMakeRange(tempDirRange.location+tempDirRange.length, newLineRange.location-tempDirRange.location-tempDirRange.length)];
+	if (workingDir == nil) {
+		workingDir = openedProjectTempDir;
+	}
+	NSString* curWorkingDirFile = [NSString stringWithFormat:@"%@/WorkingDirPath.txt", openedProjectTempDir];
+	[workingDir writeToFile:curWorkingDirFile atomically:YES encoding:NSUTF8StringEncoding error:nil];
+}
+
+- (void)saveDocument:(NSNotification*)sender {
+	
+	//NSLog(@"is open %@", [[project class] performSelector:@selector(projectFilePathWithPath:) withObject:@"/Users/kalibannez/Downloads/Browser/SCXcodeMiniMap-master/SCXcodeMinimap/SCMiniMapView.m"]);
+	
+	NSString* changedFileAbsoluteString = [[[sender object] fileURL] absoluteString];
+	NSString* changedFilePath = [changedFileAbsoluteString substringFromIndex:7];
+	
+	NSString* changedFilesStoragePath = [NSString stringWithFormat:@"%@/ChangedFiles.plist", workingDir];
+	
+	NSMutableArray* changedFiles = [NSMutableArray arrayWithContentsOfFile:changedFilesStoragePath];
+	if (changedFiles == nil) {
+		changedFiles = [NSMutableArray array];
+	}
+	
+	if ([changedFiles indexOfObject:changedFilePath] == NSNotFound) {
+		[changedFiles addObject:changedFilePath];
+		[changedFiles writeToFile:changedFilesStoragePath atomically:YES];
+	}
+//	NSLog(@"arr %@", changedFiles);
+//	NSString* command = [NSString stringWithFormat:@"echo wedfgkbgjkdf > %@", changedFilesStoragePath];
+//	system([command UTF8String]);
+	
+//	NSAlert *alert = [[NSAlert alloc] init];
+//	[alert setMessageText:[NSString stringWithFormat:@"Saving %@", changedFilesStoragePath]];
+//	[alert runModal];
+}
+
+@end
