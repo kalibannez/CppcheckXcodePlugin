f1f1b814daf423f0f5fedf6bb88d4c070f22a047
Перепелицын Александр <a.perepelitsyn@corp.mail.ru>
2014-03-15 22:38:19 +0400

    python script, docs

---
+++ IncrementalCppCheck.py
@@ -0,0 +1,79 @@
+import subprocess as sp
+import sys
+import os
+from xml.dom import minidom
+from sets import Set
+import multiprocessing
+
+def getChangedFilesFilePath() :
+	try:
+		changedFilesDir = sys.argv[1]
+	except:
+		print 'Usage: \nIncrementalCppCheck.py path_to_xcode_project_temp\n\n'
+		sys.exit(1)
+
+	changedFilesFile = changedFilesDir + '/ChangedFiles.plist'
+	if not os.path.exists(changedFilesFile):
+		sys.exit(0)
+
+	return changedFilesFile
+
+def parseChangedFiles(changedFilesFile):
+	changedFiles = []
+
+	xmldoc = minidom.parse(changedFilesFile)
+	arrayTag = xmldoc.getElementsByTagName('array')
+	for baseNode in arrayTag:
+		for changedFileNode in baseNode.childNodes:
+			if changedFileNode.nodeType == changedFileNode.ELEMENT_NODE:
+				changedFiles.append(changedFileNode.childNodes[0].data)
+
+	return changedFiles
+
+def checkChangedFiles(changedFiles):
+
+	filesForCheck = Set()
+	for changedFile in changedFiles:
+		filesForCheck.add(changedFile)
+		if changedFile.endswith('.cpp'):
+			headerForCpp = changedFile[:-4]+'.h'
+			if os.path.exists(headerForCpp ):
+				filesForCheck.add(headerForCpp)
+		if changedFile.endswith('.h') :
+			cppForHeader = changedFile[:-2]+'.cpp'
+			if os.path.exists(cppForHeader ):
+				filesForCheck.add(cppForHeader)
+		if changedFile.find('/') != -1:
+			filesForCheck.add('-I'+changedFile[:changedFile.rfind('/')])
+
+	params = list()
+	params.append('/Applications/cppckeck/cppcheck')
+
+	for fileForCheck in filesForCheck:
+		params.append(fileForCheck)
+
+	params.append('-j '+str(multiprocessing.cpu_count()))
+	params.append('--enable=warning,performance,portability')
+	params.append('-q')
+	params.append('--include=/Users/kalibannez/Desktop/macros.h')
+	params.append('--template=gcc')
+	params.append('--platform=unix32')
+	child = sp.Popen(params, stdout=sp.PIPE, stderr=sp.PIPE)
+	child.wait()
+	cppcheckOutput = child.communicate()[1]
+
+	return cppcheckOutput
+
+def replaceWarningsForErrors(cppcheckOutput) :
+	return cppcheckOutput.replace(': warning:', ': errors:')
+
+changedFilesFilePath = getChangedFilesFilePath()
+changedFiles = parseChangedFiles(changedFilesFilePath)
+cppcheckOutput = checkChangedFiles(changedFiles)
+errorsList = replaceWarningsForErrors(cppcheckOutput)
+print errorsList
+
+os.remove(changedFilesFilePath)
+
+if len(errorsList):
+	sys.exit(1)
--- README.md
+++ README.md
@@ -8,12 +8,15 @@ Plugin for Xcode that allows to make ccpcheck incremental static analysis
 - Easy to modify: logic of plugin separated for three parts:
 	- Xcode plugin, that hooks and dumps source file changes
 	- Xcode run script, that configure, run python script and interpret results
-	- Python run script, that interpret results of plugin, process it, do checks, process results and send to Xcode
+	- Python IncrementalCppCheck.py script, that interpret results of plugin, process it, do checks, process results and send to Xcode
+For example, if you want to run cppckeck with special options: just change commant line arguments in IncrementalCppCheck.py
+- Easy to use: just setup it one time and it will be work every build
+- Fast: cppcheck will be run only for changed files
 
 ## Installation
 - Install Xcode plugin by building the project and restart Xcode
 - Copy IncrementalCppCheck.py script into your project root
-- In Xcode in Build Phases tab create new Run Script and enter following script:
+- In Xcode on Build Phases tab create new Run Script and enter following script:
 	```bash
 	SCRIPT_PATH="${SRCROOT}/IncrementalCppCheck.py"
 WORKING_DIR=$(eval echo -e `<${PROJECT_TEMP_ROOT}"/WorkingDirPath.txt"`)
@@ -22,3 +25,8 @@ if [ $? -ne 0 ] ; then
     exit 1
 fi
 ```
+
+## Uninstalling
+- Remove CppcheckXcodePlugin.xcplugin from ~/Library/Application Support/Developer/Shared/Xcode/Plug-ins folder
+- Remove created for plugin run script from Xcode
+- Remove IncrementalCppCheck.py script from your project folder
